56d18d64783bffda8e827e0d806b2ce90d67de65
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2023-10-23 11:14:46 -0300

    Criar página 404
    Um problema no projeto causou a criação dessa branch

--- src/modelo/Usuario.tsx
+++ src/modelo/Usuario.tsx
@@ -2,8 +2,10 @@ import Veiculo from "./Veiculo/Veiculo";
 
 export default interface Usuario {
   img: string;
+
   email: string;
   nomeUsuario: string;
   senha: string;
-  veiculoUsado: Veiculo;
+
+  veiculoUsado?: Veiculo;
 }
