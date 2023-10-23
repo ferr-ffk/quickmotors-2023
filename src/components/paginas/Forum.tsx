361031573ebfb1ad2b04bf46b1a14d4a2daccc9f
nando <freitax.li@gmail.com>
2023-10-23 20:45:20 -0300

    Alterar tamanho do componente fórum

--- src/components/paginas/Forum.tsx
+++ src/components/paginas/Forum.tsx
@@ -61,6 +61,13 @@ function Forum() {
           data={"Há 10 minutos"}
           veiculo={"Carro"}
         />
+        <Comentario
+          usuario={"nando.lxz"}
+          titulo={"Meu carro quebrou"}
+          texto={"Ai que n sei o que n sei o que lá"}
+          data={"Há 10 minutos"}
+          veiculo={"Carro"}
+        />
       </main>
     </>
   );
